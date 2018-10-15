afa4b5ea62cfbf81a4b40e1d352cbe7b8ec90562
s33dco <code@s33d.co>
2018-10-15 12:44:22 +0100

    prepare for heroku deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 let app = express();
 
@@ -64,6 +65,6 @@ app.get('/bad', (req, res)=>{
 	})
 });
 
-app.listen(3000, () => {
-	console.log('server up on port 3000');
+app.listen(port, () => {
+	console.log(`server running on ${port}`);
 });
